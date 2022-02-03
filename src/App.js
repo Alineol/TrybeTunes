ae99107116dc791c4754c98c150d55b316d2a6b3
Aline <allinne.oliveira.ol@gmail.com>
2022-02-03 15:49:43 -0300

    Cria rotas necessárias para aplicação req01

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import NotFound from './pages/NotFound';
+import ProfileEdit from './pages/ProfileEdit';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <>
+        <p>TrybeTunes</p>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ () => <Login /> } />
+            <Route path="/search" component={ () => <Search /> } />
+            <Route path="/album/:id" component={ () => <Album /> } />
+            <Route path="/favorites" component={ () => <Favorites /> } />
+            <Route exact path="/profile" component={ () => <Profile /> } />
+            <Route exact path="/profile/edit" component={ () => <ProfileEdit /> } />
+            <Route exact path="*" component={ () => <NotFound /> } />
+
+          </Switch>
+        </BrowserRouter>
+      </>
+    );
   }
 }
 
