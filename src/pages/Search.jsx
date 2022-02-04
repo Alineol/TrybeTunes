c113e6551ffcdfc41f6d3131bb1d082decc6d789
Aline <allinne.oliveira.ol@gmail.com>
2022-02-03 21:48:23 -0300

    cria formulario de pesquisa req.05

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,51 @@ import React from 'react';
 import Header from '../components/Header';
 
 export default class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      disabled: true,
+      searchInput: '',
+    };
+  }
+
+  onchange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      searchInput: value,
+      disabled: this.checkInput(value),
+    });
+  }
+
+  checkInput = (value) => {
+    if (value.length > 1) {
+      return false;
+    } return true;
+  }
+
   render() {
+    const { disabled, searchInput } = this.state;
     return (
       <div data-testid="page-search">
-        SearchPage
         <Header />
+        SearchPage
+        <form>
+          <div>
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              onChange={ this.onchange }
+              value={ searchInput }
+            />
+            <button
+              data-testid="search-artist-button"
+              type="submit"
+              disabled={ disabled }
+            >
+              Pesquisar
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
