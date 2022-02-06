1bbc63090b6d66c05f34ca9b225335576b19c560
Aline <allinne.oliveira.ol@gmail.com>
2022-02-05 21:26:27 -0300

    remove favoritos da lista de favoritos req12

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,59 @@
 import React from 'react';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
+import Charging from '../components/Charging';
 
 export default class Favorites extends React.Component {
+  state = {
+    loading: true,
+    favorites: [],
+  }
+
+  componentDidMount() {
+    this.getList();
+  }
+
+  getList = async () => {
+    const favoriteSongs = await getFavoriteSongs();
+    this.setState({
+      loading: false,
+      favorites: favoriteSongs,
+    });
+  }
+
+  onCheck = async ({ target }) => {
+    const { favorites } = this.state;
+    const nId = Number.parseInt(target.id, 10);
+    const selected = favorites.find(({ trackId }) => trackId === nId);
+    this.setState({ loading: true });
+    await removeSong(selected);
+    await this.getList();
+  }
+
+  toRender() {
+    const { favorites } = this.state;
+    const check = true;
+    return (
+      <section className="Favorits-sec">
+        {favorites.map((music) => (
+          <MusicCard
+            key={ music.trackName }
+            music={ music }
+            onClick={ this.onCheck }
+            checked={ check }
+          />
+        ))}
+      </section>
+    );
+  }
+
   render() {
+    const { loading } = this.state;
     return (
       <div data-testid="page-favorites">
-        favoritesPage
         <Header />
+        {loading ? <Charging /> : this.toRender()}
       </div>
     );
   }
