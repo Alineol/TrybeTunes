67518dad1ae5a6aa262756a8b157b5fb664367df
Aline <allinne.oliveira.ol@gmail.com>
2022-02-06 08:01:24 -0300

    requisito 12 incompleto

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,64 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Charging from '../components/Charging';
 
 export default class Profile extends React.Component {
+  state={
+    user: [],
+    loading: true,
+  }
+
+  componentDidMount() {
+    this.callGetUser();
+  }
+
+  callGetUser = async () => {
+    const user = await getUser();
+    this.setState({
+      user,
+      loading: false,
+    });
+  }
+
+  toRender = () => {
+    const { user: { description, email, image, name } } = this.state;
+    return (
+      <section className="user-sec">
+        <div>
+          <img
+            src={ image }
+            alt="Imagem do usuario"
+            data-testid="profile-image"
+          />
+          <Link to="/profile/edit">
+            <button type="button">Editar perfil</button>
+          </Link>
+        </div>
+        <p>
+          Nome:
+          {name}
+        </p>
+        <p>
+          Email:
+          {email}
+        </p>
+        <p>
+          Descrição:
+          {description}
+        </p>
+        <div />
+      </section>
+    );
+  }
+
   render() {
+    const { loading } = this.state;
     return (
       <div data-testid="page-profile">
-        profilePage
         <Header />
+        {loading ? <Charging /> : this.toRender()}
       </div>
     );
   }
